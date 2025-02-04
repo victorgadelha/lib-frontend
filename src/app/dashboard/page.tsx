8ef6c3c65a12cb676793c94246ec00b6bf134689
João Victor Gadelha <joaovictor_gadelha@live.com>
2025-02-04 17:43:49 -0300

    feat: Add avatar component and menubar to dashboard with navigation options

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,117 @@
-import React from "react";
-import Image from "next/image";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import {
+  LuLibraryBig,
+  LuHouse,
+  LuBook,
+  LuBookUp,
+  LuClockArrowDown,
+} from "react-icons/lu";
 
 const page = () => {
   return (
-    <div
-      className="flex items-center 
-      justify-center h-screen w-full"
-    >
-      {" "}
-      <Image src="/taylor.gif" alt="Next.js logo" width={500} height={500} />
-    </div>
+    <header className="flex items-center shadow-md p-6">
+      <div className="flex items-center">
+        {" "}
+        <LuLibraryBig className="w-14 h-14 text-blue-500" />
+        <h1 className="text-4xl font-display font-bold text-gray-800">LIB+</h1>
+      </div>
+
+      <nav className="flex items-center ml-auto">
+        {" "}
+        <Menubar className="border-none shadow-none font-sans text-gray-500">
+          <MenubarMenu>
+            <MenubarTrigger className="p-3 hover:">
+              {" "}
+              <LuHouse className="size-5 mr-2" />
+              Página Inicial
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>New Window</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Share</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Print</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+
+          <MenubarMenu>
+            <MenubarTrigger className="p-3">
+              {" "}
+              <LuBook className="size-5 mr-2" />
+              Livros
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>New Window</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Share</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Print</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+
+          <MenubarMenu>
+            <MenubarTrigger className="p-3">
+              {" "}
+              <LuBookUp className="size-5 mr-2" />
+              Empréstimos
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>New Window</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Share</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Print</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+
+          <MenubarMenu>
+            <MenubarTrigger className="p-3">
+              {" "}
+              <LuClockArrowDown className="size-5 mr-2" />
+              Histórico
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>New Window</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Share</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Print</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+        <Avatar>
+          <AvatarImage
+            className="text-end"
+            src="https://github.com/shadcn.png"
+            alt="@shadcn"
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </nav>
+    </header>
   );
 };
 
